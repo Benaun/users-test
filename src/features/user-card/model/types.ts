4660134470fae4c3304e36fcd4d320b32da134c2
Benaun <andrey.kuhtin126@gmail.com>
2025-10-22 10:22:09 +0300

    feat: add zod schema for user update form, query-client, types for user card, user response, user edit form

---
+++ src/features/user-card/model/types.ts
@@ -0,0 +1,43 @@
+export interface IUserResponse {
+  id: number
+  name: string
+  username: string
+  email: string
+  address: IAddress
+  phone: string
+  website: string
+  company: ICompany
+}
+
+interface IAddress {
+  street?: string
+  suite?: string
+  city?: string
+  zipcode?: string
+  geo?: IGeo
+}
+
+interface IGeo {
+  lat: string
+  lng: string
+}
+
+interface ICompany {
+  name?: string
+  catchPhrase?: string
+  bs?: string
+}
+
+export interface IUserCard {
+  id: number
+  username: string
+  city: string
+  companyName: string
+  avatar: string | undefined
+}
+
+export interface IUserCardFull extends IUserCard {
+  name: string
+  email: string
+  phone: string
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 
 import App from './app/App.tsx'
+import { queryClient } from './common/api/query-client.ts'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>
 )
